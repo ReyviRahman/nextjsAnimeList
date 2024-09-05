2360919e9252df6718ec160b82da28b8968cf9d0
Reyvi Rahman <reyvirahman@gmail.com>
2024-09-05 15:06:48 +0700

    Adding Search Feature

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,19 @@
-import AnimeList from "./component/AnimeList";
+import AnimeList from "@/components/AnimeList";
+import Header from "@/components/AnimeList/Header";
 
-const Home = async () => {
+const Page = async () => {
 
-  const response = await fetch (`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`);
-  const anime = await response.json();
+  const response = await fetch (`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`);
+  const topAnime = await response.json();
 
   return (
-    <div>
-      <h1>Paling Populer</h1>
-      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
-        {anime.data.map( data => {
-          return (
-            <div key={data.mal_id} className="shadow-xl">
-              <AnimeList title={data.title} images={data.images.webp.image_url} id={data.mal_id}/>
-            </div>
-          )
-        })}
-      </div>
-    </div>
+    <>
+      <section>
+        <Header title={"Paling Populer"} linkTitle={"Lihat Semua"} linkHref={"/populer"} />
+        <AnimeList api={topAnime}/>
+      </section>
+    </>
   );
 }
 
-export default Home
+export default Page
---
+++ src/components/Navbar/InputSearch.js
@@ -0,0 +1,23 @@
+"use client"
+import { MagnifyingGlass } from "@phosphor-icons/react"
+import { useRouter } from "next/navigation"
+import { useRef } from "react"
+
+const InputSearch = () => {
+  const searchRef = useRef()
+  const router = useRouter()
+  const handleSearch = () => {
+    const keyword = searchRef.current.value
+    router.push(`/search/${keyword}`)
+  }
+  return (
+    <div className="relative">
+      <input placeholder="Cari Anime" className="w-full p-2 rounded" ref={searchRef}/>
+      <button className="absolute top-2 end-0" onClick={handleSearch}>
+        <MagnifyingGlass size={24} />
+      </button>
+    </div>
+  )
+}
+
+export default InputSearch
